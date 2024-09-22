26314bd956283064e5ed2b1599f82eb1ba77e6a9
Muhamadfajaryh12 <2010631170090@student.unsika.ac.id>
2024-09-22 10:47:22 +0700

    adj

--- database/migrations/2024_09_21_101941_create_attendence_table.php
+++ database/migrations/2024_09_21_101941_create_attendence_table.php
@@ -11,13 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('attendence', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
+            $table->text('foto');
+            $table->string('status');
+            $table->string('lokasi');
+            $table->unsignedBigInt('id_user');
+            $table->foreign('id_user')->references('id')->on('users');
         });
     }
 
@@ -26,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('attendence');
     }
 };
